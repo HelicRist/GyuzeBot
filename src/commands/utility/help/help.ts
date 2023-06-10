d7d7e0128f90dcc962a92a0d6515f5eb13d7956e
Rimaro03 <leonardoongaro4@gmail.com>
2023-06-10 08:45:42 +0200

    added commands to context

--- src/commands/utility/help/help.ts
+++ src/commands/utility/help/help.ts
@@ -1,5 +1,6 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { Collection, SlashCommandBuilder } from 'discord.js';
 import { IContext } from '../../../types/context';
+import { Command } from '../../../types/command';
 
 const infos = new SlashCommandBuilder()
 	.setName('help')
@@ -8,24 +9,35 @@ const infos = new SlashCommandBuilder()
 
 const help = {
 	data: infos.toJSON(),
-	async execute( ctx: IContext, interaction: any){
+	async execute(ctx: IContext, interaction: any) {
+		const fields = getCommands(ctx.commands);
 		const menu = {
 			color: 0x0099ff,
 			title: 'Help menu',
 			author: {
 				name: 'Rimaro03, Heldin',
-				iconURL: 'https://www.iconspng.com/uploads/gyoza-colour/gyoza-colour.png', 
+				iconURL: 'https://www.iconspng.com/uploads/gyoza-colour/gyoza-colour.png',
 				url: 'https://github.com/HelicRist/GyuzeBot'
 			},
 			description: 'Your fav bot Gyuza here to help!',
 			thumbnail: { url: 'https://www.iconspng.com/uploads/gyoza-colour/gyoza-colour.png' },
-			fields: [
-				{ name: 'Help', value: 'Display this help menu' }
-			]
+			fields: fields,
 		};
-		
-		return await interaction.reply({embeds: [menu]});
+		return await interaction.reply({ embeds: [menu] });
 	}
 };
 
+const getCommands = (commands: Collection<string, Command>) => {
+	const fields: unknown[] = [];
+	commands.map(command => {
+		const item = {
+			name: command.data.name.toUpperCase(),
+			value: ' `' + `/${command.data.name}`+'`'+`\n${command.data.description}`,
+			inline: true
+		};
+		fields.push(item);
+	});
+	
+	return fields;
+};
 export default help;
--- src/types/context.ts
+++ src/types/context.ts
@@ -1,6 +1,8 @@
-import { Client } from 'discord.js';
+import { Client, Collection } from 'discord.js';
+import { Command } from './command';
 
 export interface IContext {
   client: Client;
   config: object;
+  commands: Collection<string, Command>;
 }
